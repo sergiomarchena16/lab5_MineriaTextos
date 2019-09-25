7a37d6263cd6a9b62bd169fc2d38b7cfa1fcdf4d
Sergio A. Marchena G <mar16387@uvg.edu.gt>
2019-09-25 14:48:44 -0600

    update
    UPDATE

--- lab5.R
+++ lab5.R
@@ -19,13 +19,13 @@ c1_test<-c1
 #tolower(c1_test)
 
 
-
 #Eliminacion de cosas
 c1_test<-tokens(c1_test, remove_numbers = TRUE, remove_punct = TRUE, remove_separators = TRUE)
 
 
 # make a dfm, removing stopwords and applying stemming
 dfm1_test <- dfm(c1_test,remove = stopwords("english"),stem = TRUE, remove_punct = TRUE)
+
 
 dfm1<-dfm(c1_test)
 topfeatures(dfm1_test, 20) # 20 most frequent words
